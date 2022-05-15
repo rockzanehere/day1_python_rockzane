7ba99653afc5499e0fb47273ce8b38c5eb1be15e
rockzanehere <105067428+rockzanehere@users.noreply.github.com>
2022-05-15 12:46:51 +0530

    Add files via upload

---
+++ syntax.py
@@ -0,0 +1,18 @@
+from __future__ import print_function
+
+
+print("hello everyone")
+
+if 14 > 3:
+    print("fourteen is greater than three")
+
+a = 5
+b = 10
+print ("a and  b are multiples of 5 ")
+
+if 2 < 10:
+    print("2 is smaller than 10")
+
+if 3 ==0:
+    print( "3 is 0" )
+    
---
+++ variables.py
@@ -0,0 +1,44 @@
+books = 28  #counting no of books and finding type 
+articles = 89
+x = books  
+y = articles 
+print( type(x)) 
+print( type(y))
+
+#variable names cane be 
+_rockzanehere = str
+Rockzane_here_99 = str  #snake typing 
+RockzaneHere = str  #camel typing
+
+string_instrumenmts = ["violin" , "guitar", "celo"]
+a, b , c = string_instrumenmts
+indian_instruments = ["veena", "tambura" , "dolak"]
+x, y, z = indian_instruments
+print (x)
+print(a)
+print(y)
+#trying out to find whats in shelf and its type 
+shelf = ["covers","pencils", "charger",38]
+s, d, e, a = shelf
+print (d)
+print (type(e))
+print (type(a))
+
+#diferent types in writting syntax 
+j = "we"
+k = "should"
+l = "seriously"
+m = "take"
+n = "steps on "
+o = "climate change"
+print (j + k + l + m + n + o)
+
+
+#global variable 
+
+x = "vindicators"
+def myfunc ():
+    x =  "vindicators"
+    print (" rick is better than " + c )
+     
+#this code is not coming 
